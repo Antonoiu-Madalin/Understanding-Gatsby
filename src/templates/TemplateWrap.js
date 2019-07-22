b124152521315e7fc71f749bce2813b2c090c77e
@mantonoiu <antonoiumadalin@gmail.com>
2019-07-22 18:37:15 +0000

    Huh?

--- src/templates/TemplateWrap.js
+++ src/templates/TemplateWrap.js
@@ -12,25 +12,16 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-
 import ListSubheader from '@material-ui/core/ListSubheader';
-import Collapse from '@material-ui/core/Collapse';
 
 
 /*Icons*/
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import HomeIcon from '@material-ui/icons/Home'
 import InfoIcon from '@material-ui/icons/Info'
 import RssFeed from '@material-ui/icons/RssFeed'
 import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
-
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
 import Book from '@material-ui/icons/Book';
 
 const activeStyles  = {
@@ -84,12 +75,6 @@ export default function ResponsiveDrawer(props) {
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const [open, setOpen] = React.useState(true);
-
-  function handleClick() {
-    setOpen(!open);
-  }
-
   function handleDrawerToggle() {
     setMobileOpen(!mobileOpen);
   }
@@ -116,7 +101,7 @@ export default function ResponsiveDrawer(props) {
             </Link>
 
             {/* Despre */}
-            <Link to="/studiu-biblic/" className="listItemClear" activeStyle={activeStyles} partiallyActive={true}>
+            <Link to="/" className="listItemClear" /*partiallyActive={true}*/>
               <ListItem button component="a">
                 <ListItemIcon> <Book/></ListItemIcon>
                 <ListItemText primary="Studiu biblic" />
@@ -124,7 +109,7 @@ export default function ResponsiveDrawer(props) {
             </Link>
 
             {/* Contact */}
-            <Link to="/raspunsuri-rapide/" className="listItemClear" activeStyle={activeStyles} partiallyActive={true}>
+            <Link to="/" className="listItemClear" /*partiallyActive={true}*/>
               <ListItem button component="a">
                 <ListItemIcon> <MailIcon /> </ListItemIcon>
                 <ListItemText primary="Raspunsuri rapide" />
@@ -132,89 +117,15 @@ export default function ResponsiveDrawer(props) {
             </Link>
 
             {/* Blog */}
-            <Link to="/blog/" className="listItemClear" activeStyle={activeStyles} partiallyActive={true}>
+            <Link to="/" className="listItemClear" /*partiallyActive={true}*/>
               <ListItem button component="a">
                 <ListItemIcon> <RssFeed /> </ListItemIcon>
                 <ListItemText primary="Blog" />
               </ListItem>
             </Link>
 
-      </List>
-      <Divider />
-
-
-
-       <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Nested List Items
-        </ListSubheader>
-      }
-    >
-
-      <ListItem button>
-        <ListItemIcon>
-          <SendIcon />
-        </ListItemIcon>
-        <ListItemText primary="Sent mail" />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <DraftsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drafts" />
-      </ListItem>
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inbox" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary="Starred" />
-          </ListItem>
-
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Mooned" />
-          </ListItem>
-
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Delayed" />
-          </ListItem>
-
-        </List>
-      </Collapse>
-    </List>
-    <Divider />
-
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Nested List Items 2
-        </ListSubheader>
-      }
-    >
-            {/* Home */}
-            <Link to="/" className="listItemClear" activeStyle={activeStyles}>
-              <ListItem button component="a">
-                <ListItemIcon> <HomeIcon/></ListItemIcon>
-                <ListItemText primary="Acasă" />
-              </ListItem>
-            </Link>
-
             {/* Despre */}
-            <Link to="/" className="listItemClear" activeStyle={activeStyles}>
+            <Link to="/" className="listItemClear" >
               <ListItem button component="a">
                 <ListItemIcon> <InfoIcon/></ListItemIcon>
                 <ListItemText primary="Despre" />
@@ -222,22 +133,12 @@ export default function ResponsiveDrawer(props) {
             </Link>
 
             {/* Contact */}
-            <Link to="/" className="listItemClear" activeStyle={activeStyles}>
+            <Link to="/" className="listItemClear">
               <ListItem button component="a">
                 <ListItemIcon> <MailIcon /> </ListItemIcon>
                 <ListItemText primary="Contact" />
               </ListItem>
             </Link>
-
-            {/* Newsletter */}
-            <Link to="/" className="listItemClear" activeStyle={activeStyles}>
-              <ListItem button component="a">
-                <ListItemIcon> <RssFeed /> </ListItemIcon>
-                <ListItemText primary="Newsletter" />
-              </ListItem>
-            </Link>
-
-
 
       </List>
       <Divider />
